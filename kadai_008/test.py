64e35d240a9bd912b74fa109a4a5db948fe451b2
xinome <xinome2038@gmail.com>
2023-11-14 20:58:31 +0900

    課題回答

--- kadai_008/test.py
+++ kadai_008/test.py
@@ -1,1 +1,12 @@
 # 条件分岐のif文で処理を切り替えよう
+
+var = 15
+
+if (var % 3 == 0 and var % 5 == 0):
+  print("FizzBuzz")
+elif (var % 5 == 0):
+  print("Buzz")
+elif (var % 3 == 0):
+  print("Fizz")
+else:
+  print(var)
