4801f502b1775700b28cd4a8df43c0b27163093c
xinome <xinome2038@gmail.com>
2023-11-15 18:13:41 +0900

    課題回答

--- kadai_015/test.py
+++ kadai_015/test.py
@@ -1,2 +1,18 @@
 # クラスを使いこなそう
 
+# クラスを定義する
+class Human:
+  def __init__(self):
+    self.name = ""
+    self.age = 0
+
+  def printinfo(self, name, age):
+    self.name = name
+    self.age = age
+    print(self.name)
+    print(self.age)
+
+# インスタンス化する
+instance = Human()
+
+instance.printinfo("侍太郎", 25)
